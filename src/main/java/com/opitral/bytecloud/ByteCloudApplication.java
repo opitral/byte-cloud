06b1d28e59001d1a554af21f0f1dbc23be69d77c
Volodymyr Slobodian <opitral@proton.me>
2024-09-12 13:47:27 +0300

    init project

---
+++ src/main/java/com/opitral/bytecloud/ByteCloudApplication.java
@@ -0,0 +1,13 @@
+package com.opitral.bytecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/opitral/bytecloud/ByteCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opitral.bytecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
